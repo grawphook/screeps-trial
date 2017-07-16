5d9fd8728b071296be7e08b6b9ad94174b9e5e5a
Bob <grawphook@gmail.com>
2017-07-16 02:17:45 -0400

    Added repair walls to tower

--- role.tower.js
+++ role.tower.js
@@ -9,17 +9,32 @@
 
 module.exports = {
     run: function (tower) {
-        console.log('Tower code');
+        var repair = true;
+
         var target = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
         if (target != undefined) {
             tower.attack(target);
-        } else {
+        } else if (repair) {
             //
             // See about repairing some walls, just don't use too much energy
             var percentFull = (tower.energy / tower.energyCapacity) * 100;
 
-            console.log('Tower is '+ percentFull + 'percent full');
-            // tower.
+            console.log('Tower is ' + percentFull + ' percent full');
+            if (percentFull > 75) {
+                var walls = tower.room.find(FIND_STRUCTURES, {
+                    filter: (s) => s.structureType == STRUCTURE_WALL
+                });
+
+                var target = undefined;
+
+                target = creep.pos.findClosestByPath(walls, {
+                    filter: (w) => w.hits / w.hitsMax < 0.1
+                });
+
+                if (target != undefined) {
+                    tower.repair(target);
+                }
+            }
         }
     }
 };
